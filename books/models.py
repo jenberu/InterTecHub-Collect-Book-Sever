e651ee1011ae5f978a5d0b57d977ed3535e4de3e
jenberu <jemberu0970@gmail.com>
2024-11-24 15:27:29 +0300

    first commit

---
+++ books/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Bookes(models.Model):
+    title=models.CharField(max_length=30)
+    author=models.CharField(max_length=30)
+    publication_year=models.DateField()
+    isbn = models.CharField(max_length=13, unique=True)
+    
+    def __str__(self):
+        return self.titlepy 
