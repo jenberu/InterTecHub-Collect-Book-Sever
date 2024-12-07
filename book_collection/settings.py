d138f5383d7eccfcf5655e5b7b03855e0b62ea48
jenberu <jemberu0970@gmail.com>
2024-12-07 15:19:09 +0300

    updated

--- book_collection/settings.py
+++ book_collection/settings.py
@@ -53,7 +53,15 @@ MIDDLEWARE = [
 ]
 
 
-
+AUTH_USER_MODEL='accounts.User'
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+}
 
 ROOT_URLCONF = 'book_collection.urls'
 
